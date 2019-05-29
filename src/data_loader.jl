64c9f59e898ec91a7db5bbe146976ecc885cb6ed
thebhatman <manjunathbhat9920@gmail.com>
2019-05-30 04:13:03 +0530

    Data loaded as batches

--- src/data_loader.jl
+++ src/data_loader.jl
@@ -1,15 +1,17 @@
+using Base.Iterators: partition
+
 function img_load(img_name)
     img = load(img_name)
     img = Float64.(channelview(img))
     return img
 end
 
-function load_dataset(path)
+function load_dataset_as_batches(path, BATCH_SIZE)
     data = []
     for r in readdir(path)
         img_path = string(path, r)
         push!(data, img_load(img_path))
     end
     num_images = length(data)
-    return reshape(hcat(data...), 64, 64, 3, num_images)
+    return [reshape(hcat(data...), 64, 64, 3, num_images) for imgs in partition(imgs, BATCH_SIZE)]
 end 
