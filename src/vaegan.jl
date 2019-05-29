891b4f1a87a68425f3001ce944bca33b7977dd77
thebhatman <manjunathbhat9920@gmail.com>
2019-05-30 04:14:05 +0530

    Minor modifications to model for 3 channeled images

--- src/vaegan.jl
+++ src/vaegan.jl
@@ -23,7 +23,7 @@ verbose_freq = 100
 discriminator_eta = 0.0001f0
 generator_eta = 0.0001f0
 
-encoder_features = Chain(Conv((5,5), 1 => 32,leakyrelu, stride = (2, 2), pad = (2, 2)),
+encoder_features = Chain(Conv((5,5), 3 => 32,leakyrelu, stride = (2, 2), pad = (2, 2)),
 	BatchNorm(32),
 	Conv((5, 5), 32 => 64, leakyrelu, stride = (2, 2), pad = (2, 2)),
 	BatchNorm(64),
@@ -31,28 +31,30 @@ encoder_features = Chain(Conv((5,5), 1 => 32,leakyrelu, stride = (2, 2), pad = (
 	BatchNorm(128),
 	Conv((5, 5), 128 => 256, leakyrelu, stride = (2, 2), pad = (2, 2)),
 	BatchNorm(256),
-	x -> reshape(x, :, size(x, 4)))
+	x -> reshape(x, :, size(x, 4)),
+	Dense(4096, 2048),
+	x -> relu.(x))
 
-encoder_mean = Chain(encoder_features, Dense(1024, 512))
+encoder_mean = Chain(encoder_features, Dense(1024*2, 512))
 
-encoder_logsigma = Chain(encoder_features, Dense(1024, 512), x -> tanh.(x))
+encoder_logsigma = Chain(encoder_features, Dense(1024*2, 512), x -> tanh.(x))
 
-encoder_latent(x) = encoder_mean(x) + randn(512, 512) * exp.(encoder_logsigma(x)./2) 
+encoder_latent(x) = encoder_mean(x) + randn(512, 512) * exp.(encoder_logsigma(x)./2)
 
-decoder_generator = Chain(Dense(512, 32*8*28*28), 
-	x -> reshape(x, 28, 28, 256, :),
+decoder_generator = Chain(Dense(512, 32*8*4*4),
+	x -> reshape(x, 4, 4, 256, :),
 	BatchNorm(256),
 	x -> relu.(x),
-	ConvTranspose((5, 5), 256 => 128, relu; stride = (2, 2), pad = (2, 2)),
+	ConvTranspose((4, 4), 256 => 128, relu; stride = (2, 2), pad = (1, 1)),
 	BatchNorm(128),
-	ConvTranspose((5, 5), 128 => 64, relu; stride = (2, 2), pad = (2, 2)),
+	ConvTranspose((4, 4), 128 => 64, relu; stride = (2, 2), pad = (1, 1)),
 	BatchNorm(64),
-	ConvTranspose((5, 5), 64 => 32, relu; stride = (2, 2), pad = (2, 2)),
+	ConvTranspose((4, 4), 64 => 32, relu; stride = (2, 2), pad = (1, 1)),
 	BatchNorm(32),
-	ConvTranspose((5, 5), 32 => 1, relu; stride = (2, 2), pad = (2, 2)),
+	ConvTranspose((4, 4), 32 => 3, relu, stride = (2, 2), pad = (1, 1)),
 	x -> tanh.(x))
 
-discriminator_featuremap = Chain(Conv((5, 5), 1 => 64, leakyrelu, stride = (2, 2), pad = (2, 2)),
+discriminator_featuremap = Chain(Conv((5, 5), 3 => 64, leakyrelu, stride = (2, 2), pad = (2, 2)),
 	Conv((5, 5), 64 => 128, leakyrelu, stride = (2, 2), pad = (2, 2)),
 	BatchNorm(128),
 	Conv((5, 5), 128 => 256, leakyrelu, stride = (2, 2), pad = (2, 2)),
@@ -62,18 +64,6 @@ discriminator_featuremap = Chain(Conv((5, 5), 1 => 64, leakyrelu, stride = (2, 2
 	# x -> reshape(x, :, size(x, 4)))
 
 decoder = Chain(discriminator_featuremap, BatchNorm(256), x -> reshape(x, :, size(x, 4)),
-	Dense(1024, 1), x -> sigmoid.(x))
+	Dense(1024*4, 1), x -> sigmoid.(x))
 
 opt_encoder = ADAM(0.0003, (0.9, 0.999))
-
-
-
-
-
-	
-
-
-	
-
-
-
