498b1cb03ffc9cc5703482d66a847ae61b0b33a3
thebhatman <manjunathbhat9920@gmail.com>
2019-06-01 17:25:38 +0530

    Changed permutedims to reshape and hcat to cat

--- src/data_loader.jl
+++ src/data_loader.jl
@@ -4,7 +4,7 @@ using Images
 function img_load(img_name)
     img = load(img_name)
     img = imresize(img, 64, 64)
-    img = permutedims(Float64.(channelview(img)), [2, 3, 1])
+    img = reshape(Float64.(channelview(img)), 64, 64, 3)
     return img
 end
 
@@ -18,7 +18,7 @@ function load_dataset_as_batches(path, BATCH_SIZE)
     #println(num_images)
     batched_data = []
     for x in partition(data, BATCH_SIZE)
-        x = reshape(hcat(x...), 64, 64, 3, BATCH_SIZE)
+        x = reshape(cat(x..., dims = 4), 64, 64, 3, BATCH_SIZE)
         push!(batched_data, x)
     end
     return batched_data
